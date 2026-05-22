4da232d41095d8a52dba2a8f30a69625b1950539
Junaidkhanktk <muhammmadjunaid.cdz@gmail.com>
2026-05-22 10:18:39 +0500

    Add maven-publish configuration for JitPack (group/version + release publication)

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,35 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
 }
+
+// Set default coordinates for publications. JitPack will normally build from a git tag
+// and use that tag as the version, but having sensible defaults here is helpful for
+// local publishing and for the maven-publish configuration below.
+group = "com.github.junaidkhan08ktk"
+version = "0.0.1-SNAPSHOT"
+
+// Configure publishing for Android library modules. This applies the `maven-publish`
+// plugin and creates a `release` Maven publication that publishes the AAR produced
+// by the Android Gradle Plugin. We do this in `afterEvaluate` because the Android
+// `components["release"]` is only available after the Android plugin configures the project.
+subprojects {
+    afterEvaluate {
+        if (plugins.hasPlugin("com.android.library")) {
+            apply(plugin = "maven-publish")
+
+            publishing {
+                publications {
+                    create("release", org.gradle.api.publish.maven.MavenPublication::class) {
+                        // Use project coordinates by default: groupId = root group, artifactId = project name
+                        groupId = project.group.toString()
+                        artifactId = project.name
+                        version = project.version.toString()
+
+                        // Publish the AAR produced by the Android library plugin
+                        from(components["release"])
+                    }
+                }
+            }
+        }
+    }
+}
